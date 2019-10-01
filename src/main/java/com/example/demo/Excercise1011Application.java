7eb9e0b9994f2ee939c581a56e00cfba5a25076f
GBTC440005UR <GBTC440005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-30 20:52:11 -0400

    Excercise101

---
+++ src/main/java/com/example/demo/Excercise1011Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise1011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excercise1011Application.class, args);
+    }
+
+}
